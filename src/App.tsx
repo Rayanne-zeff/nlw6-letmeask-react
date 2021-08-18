0d2474244ef67cbd29b0c1d2957729946ab18256
Glória Rayane Zeferino <gloria_zeferino@hotmail.com>
2021-08-17 23:38:02 -0300

    Criação da sala de perguntas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route} from 'react-router-dom';
+import { ToastContainer } from "react-toastify"; // Importamos o Toastify
 
 import {Home} from "./pages/Home";
 import {NewRoom} from "./pages/NewRoom";
 import {Room} from "./pages/Room";
 
-import {AuthContextProvider} from './contexts/AuthContext'
+import {AuthContextProvider} from './contexts/AuthContext';
 
+import "react-toastify/dist/ReactToastify.css"; // O estilo do Toastify
 
 function App() {
 
     return (
         <BrowserRouter>
+            <ToastContainer autoClose={3000} />
             <AuthContextProvider>
                 <switch>
                     <Route path="/" exact component={Home}/>
